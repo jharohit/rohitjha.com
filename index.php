f8eb8a0909f7303feeedd871a2ebf9aa87f54a23
Rohit Jha <rj@rohitjha.com>
2015-04-22 19:34:52 +0530

    Moved the main nav to left top right

--- index.php
+++ index.php
@@ -1,69 +1,54 @@
 <!DOCTYPE HTML>
 <html>
-	<head>
-		<meta name="description" content="This is the personal website of Rohit Jha. This provides a gateway to his projects, ideas and creativity.">
-		<title>Rohit Jha</title>
-		<link href='http://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'>
-        <link rel="icon"  type="image/png"   href="favicon.ico">
-		<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
-		<link type="text/css" rel="stylesheet" href="style.css">
-	</head>
-	<body>
-        <div class="logo">
-            <img src="logo.png" />
-        </div>
-		<ul class="ca-menu">
-                    <li>
-                        <a href="http://www.rohitjha.com">
-                            <span class="ca-icon"><i class="icon-home icon-large"></i></span> 
-       
+<head>
+  <meta name="description" content="This is the personal website of Rohit Jha. This provides a gateway to his projects, ideas and creativity.">
+  <title>Rohit Jha</title>
+  <link href='http://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'>
+  <link rel="icon"  type="image/png"   href="favicon.ico">
+  <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+  <link type="text/css" rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="logo">
+        <img src="logo.png" />
+    </div>
+    <ul class="ca-menu">
+        <li>
+            <a href="http://www.rohitjha.com/labs" target="_blank"><i class="icon-beaker icon-large"></i>Labs</a>
+        </li>
+        <li>
+            <a href="http://www.rohitjha.com/blog" target="_blank"><i class="icon-edit icon-large"></i>Blog</a>
+        </li>
+        <li>
+            <a href="mailto:rohitjha@outlook.com" target="_blank"><i class="icon-envelope-alt icon-large"></i>Contact</a>
+        </li>
 
-                        </a>
-                    </li>
-                    <li>
-                        <a href="http://www.rohitjha.com/labs" target="_blank">
-                            <span class="ca-icon"><i class="icon-beaker icon-large"></i></span>
+    </ul>
 
-                        </a>
-                    </li>
-                    <li>
-                        <a href="http://www.rohitjha.com/blog" target="_blank">
-                            <span class="ca-icon"><i class="icon-edit icon-large"></i></span>
-
-                        </a>
-                    </li>
-                    <li>
-                        <a href="mailto:rohitjha@outlook.com" target="_blank">
-                            <span class="ca-icon"><i class="icon-envelope-alt icon-large"></i></span>
-
-                        </a>
-                    </li>
-                    
-            </ul>
-            <ul class="social-menu">            	
-            	<li>
-            		<a class="twitter" href="http://twitter.com/jharohit" target="_blank"><i class="icon-twitter icon-large"></i>twitter</a>
-            	</li>
-            	<li>
-            		<a class="facebook" href="http://facebook.com/jharohit" target="_blank">facebook</a>
-            	</li>
-            	<li>
-            		<a class="google-plus" href="https://plus.google.com/116359256653774371112/posts" target="_blank"><i class="icon-google-plus-sign icon-large"></i> Google+</a>
-            	</li>
-            	<li>
-            		<a class="linkedin" href="http://www.linkedin.com/in/rohitrj" target="_blank">Linked<i class="icon-linkedin"></i></a>
-            	</li>
-            	<li>
-            		<a class="github" href="http://github.com/jharohit" target="_blank"><i class="icon-github icon-large"></i>github</a>
-            	</li>
-            	<li>
-            		<a class="pinterest" href="http://pinterest.com/jharohit/" target="_blank"><i class="icon-pinterest icon-large"></i>interest</a>
-            	</li>
-            </ul>
+    <ul class="social-menu">            	
+     <li>
+      <a class="twitter" href="http://twitter.com/jharohit" target="_blank"><i class="icon-twitter icon-large"></i>twitter</a>
+  </li>
+  <li>
+      <a class="facebook" href="http://facebook.com/jharohit" target="_blank">facebook</a>
+  </li>
+  <li>
+      <a class="google-plus" href="https://plus.google.com/116359256653774371112/posts" target="_blank"><i class="icon-google-plus-sign icon-large"></i> Google+</a>
+  </li>
+  <li>
+      <a class="linkedin" href="http://www.linkedin.com/in/rohitrj" target="_blank">Linked<i class="icon-linkedin"></i></a>
+  </li>
+  <li>
+      <a class="github" href="http://github.com/jharohit" target="_blank"><i class="icon-github icon-large"></i>github</a>
+  </li>
+  <li>
+      <a class="pinterest" href="http://pinterest.com/jharohit/" target="_blank"><i class="icon-pinterest icon-large"></i>interest</a>
+  </li>
+</ul>
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
   ga('create', 'UA-40748599-1', 'rohitjha.com');
@@ -71,5 +56,5 @@
 
 </script>
 
-	</body>
+</body>
 </html>
